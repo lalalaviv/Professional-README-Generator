f07a45a8f8d44677404221481a6404a58d7b0955
lalalaviv <lalala.viv@hotmail.com>
2022-10-10 14:31:02 +1100

    added to question array and created writefile in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,26 +1,151 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// An array of questions for user input
 const questions = [
+    // GitHub username
     {
-       "What is your GitHub username?"
+        name: 'username',
+        type: 'input',
+        message: "What is your GitHub username?",
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+    // User Email
+    {
+        name: 'contact',
+        type: 'input',
+        message: "What is your email address?",
+        validate: contactInput => {
+            if (contactInput) {
+                return true;
+            } else {
+                console.log('Please provide your email address!');
+                return false;
+            }
+        }
+    },
+    // Project title
+    {
+        name: 'title',
+        type: 'input',
+        message: "What is the title of your project?",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to provide the project title!');
+                return false;
+            }
+        }
     },
     {
-        "What is your email address?"
+    // Project description
+        name: 'description',
+        type: 'input',
+        message: "Please write a short description of your project",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to provide a project description!');
+                return false;
+            }
+        }
     },
     {
-        "What is your project's name?"
+        name: 'license',
+        type: 'list',
+        message: "Choose a license for your project",
+        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('You need to pick the license for your project!');
+                return false;
+            }
+        }
     },
     {
-        "Please write a short description of your project"
+        name: 'installation',
+        type: 'input',
+        message: "What command should be run to install dependencies?",
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('You need to provide installation info to continue!');
+                return false;
+            }
+        }
+    },
+    {
+        name: 'tests',
+        type: 'input',
+        message: "What command should be run to test this project?",
+        validate: testsInput => {
+            if (testsInput) {
+                return true;
+            } else {
+                console.log('Please enter how to test this project!');
+                return false;
+            }
+        }
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: "What does the user need to know about using the repo?",
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to use this project!');
+                return false;
+            }
+        }
+    },
+    {
+        name: 'contribution',
+        type: 'input',
+        message: "How can people contribute to the repo?",
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('You need to provide details on how others can contribute!');
+                return false;
+            }
+        }
     }
 ];
 
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log(err) : console.log('Success! Information transferred to README file!')
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(function (userInput) {
+            console.log(userInput)
+            writeToFile("README.md", generateMarkdown(userInput));
+        });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (${data.license} === "MIT")
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+  
+
+
+  [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,6 +23,48 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## License
+----
+${data.license}
+
+## Description
+----
+${data.description}
+
+## Table of Contents
+----
+- [License](#license)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribute](#credits)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+----
+${data.installation}
+
+## Usage
+----
+${data.usage}
+
+
+## How to Contribute
+----
+${data.contribution}
+
+## Tests
+----
+${data.tests}
+
+## Quesitons
+----
+
+GitHub: [@${data.username}](https://github.com/${data.username})
+
+Email: ${data.contact}
 
 `;
 }
